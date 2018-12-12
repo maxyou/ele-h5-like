1fbf29f7538cd41d7e92346e2ad10c5d34f47f5b
maxyou <hyyou@foxmail.com>
2018-12-12 11:57:09 +0800

    sales

--- src/businesses/sales/sales.js
+++ src/businesses/sales/sales.js
@@ -12,7 +12,7 @@ class sales extends React.Component {
 
     httpUpdate(){
 
-        console.log(this.props)
+        // console.log(this.props)
 
         axios.get('/shopping/v2/menu?restaurant_id='+this.props.match.params.id)
         // axios.get('/shopping/v2/menu',{
@@ -23,11 +23,25 @@ class sales extends React.Component {
             .then(res=>{
                 if(res.status===200){
                     console.log('axios 200')
-                    console.log(res)
-                    this.setState(res.data)
+                    // console.log(res)
+
+                    this.setState({
+                        foods0:this.extFoods(res.data[0].foods),
+                        foods1:this.extFoods(res.data[1].foods)
+                    })
                 }
                 // console.log(res)
             })
+    }
+
+    extFoods(foods){
+        let extFoods = []
+        for(let i=0;i<50;i++){
+            extFoods[i] = {}
+            extFoods[i].foods = foods[i%3]
+            extFoods[i].id = i
+        }
+        return extFoods
     }
 
     componentDidMount() {
@@ -35,8 +49,25 @@ class sales extends React.Component {
     }
     
     render() {
-        
+        // console.log(this.state)
+
+        if(this.state){
+
+            return (<div>
+
+                {
+                    this.state.foods0.map((item)=><div key={item.id}>{item.foods.name}:{item.id}</div>)
+                }
+                {
+                    this.state.foods1.map((item)=><div key={item.id}>{item.foods.name}:{item.id}</div>)
+                }
+
+            </div>)
+
+
+        }else{
             return <div>no data</div>
+        }
     }
 }
 
